4ff26ce977ae5457e39223b88253a8116b52bda4
Paweł Tryfon <pawelxtryfon@gmail.com>
2018-01-21 20:17:38 +0100

    Added PATH file, default footer, header and css file.

---
+++ app/PATH.php
@@ -0,0 +1,3 @@
+<?php
+define('CSS_PATH', 'http://localhost/projekty/php/mvc_routing/public/styles');
+?>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,7 @@
+<?php
+
+require_once '../app/assets/html_defaults/header.php' ?>
+
 hello <?php echo $data['name'] ?>
+
+<?php require_once '../app/assets/html_defaults/footer.php' ?>
