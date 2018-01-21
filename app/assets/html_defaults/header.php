6b25f01b0692267461dfd7f03d4809da9c560c2f
Paweł Tryfon <pawelxtryfon@gmail.com>
2018-01-22 00:04:39 +0100

    Flex + some changes

--- app/assets/html_defaults/header.php
+++ app/assets/html_defaults/header.php
@@ -2,6 +2,23 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>/style.css">
     </head>
     <body>
+
+    <div id="header">
+
+        <div id="page-title">
+            Blik
+        </div>
+
+        <div id="menu">
+            <div class="menu-button">Przegląd routingu</div>
+            <div class="menu-button">Galeria</div>
+            <div class="menu-button">goodbye!</div>
+            <div class="menu-button">hello there</div>
+        </div>
+
+
+    </div>
--- app/init.php
+++ app/init.php
@@ -4,5 +4,4 @@ require_once 'core/App.php';
 require_once 'core/Controller.php';
 require_once 'PATH.php';
 
-
 ?>
